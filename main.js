c1eee0ba96c3a165ec2d07abc4c74ff0bcba172b
Jamie Augustin <jamie.augustin@outlook.com>
2025-09-30 14:04:12 +0200

    Fixed bug that you delete wrong task when searching for a specific task. The project now uses vite and Tailwind & AlpineJS is installed as a module.

---
+++ main.js
@@ -0,0 +1,45 @@
+import Alpine from 'alpinejs'
+import sort from '@alpinejs/sort'
+
+Alpine.plugin(sort)
+
+console.log('test')
+
+Alpine.data('todo', () => ({
+    tasks: ['Clean room', 'Read 10 Sites', 'Learn AlpineJS & Tailwind'],
+    search: '',
+
+    get filteredTasks()
+    {
+        return this.tasks.filter(
+            i => i.toLowerCase().includes(this.search.toLowerCase())
+        )
+    },
+
+    addTask()
+    {
+        for (let i = 0; i < this.tasks.length; i++) {
+            if (this.tasks[i] === this.search) {
+                return true
+            }
+        }
+        this.tasks.push(this.search)
+    },
+
+    deleteTask(task)
+    {
+        const deletedTask = task
+        console.log(deletedTask)
+        for (let i = 0; i < this.tasks.length; i++) {
+            if (this.tasks[i] === deletedTask) {
+                this.tasks.splice(i, 1)
+            }
+        }
+    }
+}))
+
+window.Alpine = Alpine
+
+Alpine.start()
+
+console.log('Alpine')
