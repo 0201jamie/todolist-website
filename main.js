d169e16f8d58eb294bb080e3091b257e571ffa3f
Jamie Augustin <jamie.augustin@outlook.com>
2025-10-02 11:53:49 +0200

    You now see how many tasks are in your list and you can edit the task name as well as the description which is not implemented yet.

--- main.js
+++ main.js
@@ -8,10 +8,16 @@ Alpine.store('todo', {
     tasks: { },
     isListFilled: false,
     showList: true,
+    editId: '',
+    editLabel: '',
 
     // TODO: Implement Function that checks if there are active tasks
     checkIfListIsFilled() {
         // const taskArrayLength = Object.keys(this.tasks).length
+    },
+
+    taskLength(){
+        return Object.keys(this.tasks).length
     },
 
     get filteredTasks()
@@ -22,16 +28,31 @@ Alpine.store('todo', {
     },
 
     addTask(taskId, taskLabel, taskActive) {
-        if (taskLabel !== '') {
-            const newTask = { id: taskId, label: taskLabel, active: taskActive }
-
-            this.tasks[taskId] = newTask;
-            this.search = ''
+        if (taskLabel === '') {
+            alert('Your task name is empty')
+            return
         }
+        const newTask = { id: taskId, label: taskLabel, active: taskActive }
+
+        this.tasks[taskId] = newTask;
+        this.search = ''
     },
 
     deleteTask(task) {
         delete this.tasks[task.id]
+    },
+
+    shareTaskData(taskId, taskLabel) {
+        this.editId = taskId
+        this.editLabel = taskLabel
+    },
+
+    updateTaskData(taskId, taskLabel) {
+        if (taskLabel === '') {
+            alert('Your task name is empty')
+            return
+        }
+        this.tasks[taskId].label = taskLabel
     },
 
     moveFinishedTask(task) {
